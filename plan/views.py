b50342aa87cbc3243baa050f3bc60ee5738a86f6
Kevin_Xi <kevin.xgr@gmail.com>
2014-03-14 17:48:07 +0800

    finish the demo

--- plan/views.py
+++ plan/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render_to_response
+import time
 
 def test(request):
-    return render_to_response("plan/demo.html", {'string': 'Kevin', })
+    temp = getTemp();
+    alert = 0
+
+    if temp > 50:
+        alert = 1
+    else:
+        alert = 0
+
+    return render_to_response("plan/demo.html", {'string': 'Kevin', 'alert': alert, 'temp': temp})
+
+def getTemp():
+    with open('temp') as f:
+        return int(f.readline())
